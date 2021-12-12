f236da91865fef9e47bccbb10bb0d42bfc10cd27
Koen Dirk van Esterik <info@koendirkvanesterik.com>
2021-12-12 16:53:28 +0100

    feat(components): add responsive behaviour to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,21 +9,15 @@ import { Container } from 'components/Container'
 import { MainMenu } from './components/MainMenu'
 
 export const Header: React.FunctionComponent = () => (
-  <header
-    className={classNames([
-      'bg-warmGray-900',
-      'fixed',
-      'py-10',
-      'w-full',
-      'z-20',
-    ])}
-  >
+  <header className={classNames(['fixed', 'py-10', 'w-full', 'z-10'])}>
     <Container>
       <nav role="navigation">
         <div className={classNames(['grid', 'grid', 'grid-cols-2', 'gap-4'])}>
-          <Link className={classNames('text-emerald-400')} to="/">
-            <Label id="title" />
-          </Link>
+          <div className={classNames(['inline-block'])}>
+            <Link to="/">
+              <Label id="title" />
+            </Link>
+          </div>
           <MainMenu />
         </div>
       </nav>
