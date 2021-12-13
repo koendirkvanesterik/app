1adacc3773a893a6808aa9406608d694908cdbd5
Koen Dirk van Esterik <info@koendirkvanesterik.com>
2021-12-13 20:08:31 +0100

    feat(components): add responsive font size and padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,16 +9,20 @@ import { Container } from 'components/Container'
 import { MainMenu } from './components/MainMenu'
 
 export const Header: React.FunctionComponent = () => (
-  <header className={classNames(['fixed', 'py-10', 'w-full', 'z-10'])}>
+  <header className={classNames(['fixed', 'pt-5', 'w-full', 'z-10'])}>
     <Container>
       <nav role="navigation">
-        <div className={classNames(['grid', 'grid', 'grid-cols-2', 'gap-4'])}>
+        <div
+          className={classNames(['grid', 'grid', 'gap-4', 'sm:grid-cols-2'])}
+        >
           <div className={classNames(['inline-block'])}>
             <Link to="/">
               <Label id="title" />
             </Link>
           </div>
-          <MainMenu />
+          <div className={classNames(['hidden', 'sm:block'])}>
+            <MainMenu />
+          </div>
         </div>
       </nav>
     </Container>
