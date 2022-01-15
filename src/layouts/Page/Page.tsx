214746248d15988cb71bd5c2b6323f86b3b62816
Koen Dirk van Esterik <info@koendirkvanesterik.com>
2022-01-15 17:27:53 +0100

    feat(pages): add page layout and content

--- src/layouts/Page/Page.tsx
+++ src/layouts/Page/Page.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-
 import { Label } from '@koendirkvanesterik/label'
 import { MDXProvider } from '@mdx-js/react'
-
 import { Arrow } from 'components/Arrow'
+import { BlogList } from 'components/BlogList'
 import { Cover } from 'components/Cover'
 import { Link } from 'components/Link'
+import { Spacer } from 'components/Spacer'
+import { WorkList } from 'components/WorkList'
+import React from 'react'
 
 const Page: React.FunctionComponent = ({ children }) => (
   <MDXProvider
     components={{
       a: Link,
       Arrow,
+      BlogList,
       Cover,
       Label,
+      Spacer,
+      WorkList,
     }}
   >
     {children}
